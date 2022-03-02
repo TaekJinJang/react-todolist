9bd10636e7a8bf1e65105315ae60eb1bf11dc5a3
Taekjin Jang <xorwls1500@naver.com>
2022-03-02 18:29:06 +0900

    3-02 lab

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import TodoCreate from "./components/TodoCreate";
 import Calender from "./calender";
 import TodoMenus from "./components/TodoMenus";
 import { FaHome } from "react-icons/fa";
+import { BsFillCalendarCheckFill } from "react-icons/bs";
 const GlobalStyle = createGlobalStyle`
 body{
   background : #e9ecef;
@@ -25,14 +26,13 @@ function App() {
         <TodoList />
         <div className="footer_bar">
           <div className="f_con home">
-            <div className="img-width">ssasdasd</div>
-            <FaHome size="500" />
-            <p>홈</p>
+            <span className="menus">
+              <FaHome size={35} />
+            </span>
           </div>
           <div className="f_con">
-            <span>
-              <FaHome />
-              <p>홈</p>
+            <span className="menus">
+              <BsFillCalendarCheckFill size={30} />
             </span>
           </div>
         </div>{" "}
