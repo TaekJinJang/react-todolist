549633c58c3889d400ad98254ee824593743da68
TaekJin Jang <xorwls1500@naver.com>
2022-03-09 18:29:48 +0900

    .

--- src/index.js
+++ src/index.js
@@ -11,22 +11,19 @@ const initialTodos = [
   {
     id: 1,
     title: "프로젝트 생성하기",
-    date: "2022-03-06",
+    date: "2022-03-09",
     done: true,
   },
   {
     id: 2,
     title: "컴포넌트 스타일링하기",
+    date: "2022-03-08",
     done: true,
   },
   {
     id: 3,
     title: "Context 만들기",
-    done: false,
-  },
-  {
-    id: 4,
-    title: "기능 구현하기",
+    date: "2022-03-01",
     done: false,
   },
 ];
